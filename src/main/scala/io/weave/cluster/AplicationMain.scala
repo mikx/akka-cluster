b7a2fe5facaf67d97c8663d43fbb73c8c8bfc1c2
mikx <mm@izba.com>
2017-05-23 20:26:02 -0400

    config update

--- src/main/scala/io/weave/cluster/AplicationMain.scala
+++ src/main/scala/io/weave/cluster/AplicationMain.scala
@@ -4,13 +4,23 @@ package io.weave.cluster
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
+import com.typesafe.config.ConfigFactory
+import com.typesafe.config.ConfigValueFactory
+
 import akka.actor.ActorSystem
-import akka.event.Logging
 import akka.actor.Props
+import akka.event.Logging
 
 object ApplicationMain extends App {
+
+  val config = ConfigFactory.load()
+  val role = config.getStringList("akka.cluster.roles").get(0)
+  val port = if (role == "SEED") "2552" else "0"  
   
-  val system = ActorSystem("ClusterSystem")
+  val overrideConfig = ConfigFactory.empty()
+    .withValue("akka.remote.netty.tcp.port", ConfigValueFactory.fromAnyRef(port))
+  
+  val system = ActorSystem("ClusterSystem", overrideConfig.withFallback(config))
   val simpleClusterListener = system.actorOf(Props(classOf[SimpleClusterListener]), "SimpleClusterListener")
   
   Logging(system, getClass).info("Seed server is up and running")
